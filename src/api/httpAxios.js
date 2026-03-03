63b0888af5fcbd2088e25e528888c2a7cb868a47
Nguyen Minh Phu <minhphu25102005@gmail.com>
2026-03-04 02:50:35 +0700

    connect to render backend

--- src/api/httpAxios.js
+++ src/api/httpAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080/api"; // đổi theo backend của bạn
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 const httpAxios = axios.create({
   baseURL: BASE_URL,
